7257753d02c10b4f4f5af719822193bc9cfdb347
Aayush Pagare <aayushpagare21@gmail.com>
2026-01-28 22:07:03 +0530

    changes

--- components/landing/index.tsx
+++ components/landing/index.tsx
@@ -33,10 +33,6 @@ export const LandingComponent = ({
       <div className="max-w-4xl mx-auto px-4 py-12 space-y-9 md:space-y-18">
          <HeroImage /> 
 
-        <RevealSection>
-          <Hero />
-        </RevealSection>  
-
          <RevealSection delay={0.1}>
           <FreelanceServices />
         </RevealSection>
@@ -48,6 +44,10 @@ export const LandingComponent = ({
         <RevealSection delay={0.1}>
           <ClientTestimonials />
         </RevealSection>
+
+        <RevealSection>
+          <Hero />
+        </RevealSection>  
 
         <RevealSection delay={0.1}>
           <LatestPosts latestPosts={latestPosts} />
