b3a17639c9f1e8dff3cf65d0c60dfd4576841460
Aayush Pagare <aayushpagare21@gmail.com>
2026-03-16 10:32:31 +0530

    scripts in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/navbar";
@@ -28,73 +27,70 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className={inter.variable}>
-   <head>
-
-  <Script id="optimeleon-cookiebot" strategy="beforeInteractive" dangerouslySetInnerHTML={{__html: `<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="7667d368-e0ce-4baa-b12f-bf07310ae6c3" data-blockingmode="auto" type="text/javascript"></script>`}}/>
-    
-  
-  {/* Optimeleon Cookie Consent */}
-  <Script
-    id="opti-cookie-consent"
-    strategy="beforeInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.setOptiCookieConsent = function(consent) {
-          localStorage.setItem("opti_consent", consent);
-        };
-      `,
-    }}
-  />
-  
-  {/* Optimeleon Overlay */}
-  <Script
-    id="optimeleon-overlay"
-    strategy="beforeInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        !(function (h, i, e) {
-          var t = 2000;
-          var n = h.createElement("style");
-          n.id = e;
-          n.innerHTML = "body{opacity:0}";
-          h.head.appendChild(n);
-          i.rmfk = function () {
-            var t = h.getElementById(e);
-            t && t.parentNode.removeChild(t);
-          };
-          setTimeout(i.rmfk, t);
-        })(document, window, "optimeleon-overlay");
-      `,
-    }}
-  />
-  
-  {/* Google Tag Manager */}
-  <Script
-    id="gtm-script"
-    strategy="beforeInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-M9FQVCPW');
-      `,
-    }}
-  />
-  
-  {/* Optimeleon Initialization */}
-  <Script
-    id="optimeleon-init"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        !function(e,t,n,o,c,a,m){e.optimeleon||(c=e.optimeleon=function(){c.callMethod?c.callMethod.apply(c,arguments):c.queue.push(arguments)},c.push=c,c.queue=[],(a=t.createElement(n)).async=!0,a.src="https://cdn-stag.optimeleon.com/mcf-m55jl/aay-1mvcw/v1.main.js",(m=t.getElementsByTagName(n)[0]).parentNode.insertBefore(a,m))}(window,document,"script");
-        optimeleon("init",true,true);
-      `,
-    }}
-  />
-</head>
+      <head>
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="7667d368-e0ce-4baa-b12f-bf07310ae6c3" data-blockingmode="auto" type="text/javascript"></script>
+        
+        <script
+          id="opti-cookie-consent"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.setOptiCookieConsent = function(consent) {
+                localStorage.setItem("opti_consent", consent);
+              };
+            `,
+          }}
+        />
+        
+        <script
+          id="optimeleon-overlay"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !(function (h, i, e) {
+                var t = 2000;
+                var n = h.createElement("style");
+                n.id = e;
+                n.innerHTML = "body{opacity:0}";
+                h.head.appendChild(n);
+                i.rmfk = function () {
+                  var t = h.getElementById(e);
+                  t && t.parentNode.removeChild(t);
+                };
+                setTimeout(i.rmfk, t);
+              })(document, window, "optimeleon-overlay");
+            `,
+          }}
+        />
+        
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          id="gtm-script"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M9FQVCPW');
+            `,
+          }}
+        />
+        
+        <script
+          id="optimeleon-init"
+          type="text/javascript"
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,o,c,a,m){e.optimeleon||(c=e.optimeleon=function(){c.callMethod?c.callMethod.apply(c,arguments):c.queue.push(arguments)},c.push=c,c.queue=[],(a=t.createElement(n)).async=!0,a.src="https://cdn-stag.optimeleon.com/mcf-m55jl/aay-1mvcw/v1.main.js",(m=t.getElementsByTagName(n)[0]).parentNode.insertBefore(a,m))}(window,document,"script");
+              optimeleon("init",true,true);
+            `,
+          }}
+        />
+      </head>
 
 
       <body className="bg-background text-foreground antialiased">
