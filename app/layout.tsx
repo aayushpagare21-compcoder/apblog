5fa69b989b9c9379bf87411eb5b242f6d4661b14
Aayush Pagare <aayushpagare21@gmail.com>
2026-07-22 21:38:32 +0530

    update Optimeleon loader script URLs in layout to new ID cdz5ubaRcEvc

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,10 +73,10 @@ export default function RootLayout({
             __html: `window.optimeleon=window.optimeleon||function(){(optimeleon.q=optimeleon.q||[]).push(arguments);return{ok:true,verb:String(arguments[0]||''),error:'queued'}};window.__opti_bus="__opti_capture";window.__opti_capture=window.__opti_capture||function(){(__opti_capture.q=__opti_capture.q||[]).push(arguments)};setTimeout(function(){var s=document.getElementById('__opti_af');if(s)s.remove()},300);`,
           }}
         />
-        <script async src="https://edge-staging.optimeleon.com/b/BEhPHMx4VQ0u.js" />
+        <script async src="http://localhost:8787/b/cdz5ubaRcEvc.js" />
         <script
           async
-          src="https://edge-staging.optimeleon.com/c/BEhPHMx4VQ0u.js"
+          src="http://localhost:8787/c/cdz5ubaRcEvc.js"
           data-consent-default="opt-in"
         />
 
